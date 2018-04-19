0a2ddb379b70daf875afa7ee18ed8f78836697d2
Dao Liu <daoliu@amazon.com>
2018-04-19 18:56:13 +0800

    show device info

--- js/motion_detect.js
+++ js/motion_detect.js
@@ -3,10 +3,10 @@ if (window.DeviceMotionEvent) {
     alert('support DeviceMotionEvent');
     var index = 0;
     window.addEventListener('devicemotion', function (event) {
-        if (index++ <= 10) {
-            return;
-        }
-        index = 0;
+        // if (index++ <= 10) {
+        //     return;
+        // }
+        // index = 0;
         // var acc = event.accelerationIncludingGravity || {};
         // var rr = event.rotationRate || {};
         // var str = 'devicemotion:<br>';
@@ -24,20 +24,20 @@ else {
     $('.motion-info').html('DO NOT support DeviceMotionEvent');
 }
 
-if (window.DeviceOrientationEvent) {
-    var index = 0;
-    window.addEventListener('deviceorientation', function (event) {
-        if (index++ <= 10) {
-            return;
-        }
-        index = 0;
-        var str = 'deviceorientation :<br>';
-        str += 'alpha:' + event.alpha + '<br>';
-        str += 'beta:' + event.beta + '<br>';
-        str += 'gamma:' + event.gamma + '<br>';
-        $('.orientation-info').html(str);
-    }, false);
-}
-else {
-    $('.orientation-info').html('DO NOT support DeviceOrientationEvent');
-}
+// if (window.DeviceOrientationEvent) {
+//     var index = 0;
+//     window.addEventListener('deviceorientation', function (event) {
+//         if (index++ <= 10) {
+//             return;
+//         }
+//         index = 0;
+//         var str = 'deviceorientation :<br>';
+//         str += 'alpha:' + event.alpha + '<br>';
+//         str += 'beta:' + event.beta + '<br>';
+//         str += 'gamma:' + event.gamma + '<br>';
+//         $('.orientation-info').html(str);
+//     }, false);
+// }
+// else {
+//     $('.orientation-info').html('DO NOT support DeviceOrientationEvent');
+// }
