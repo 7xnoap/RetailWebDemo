3d14f3ce85e05257054e5548703ad3e16ba916d4
Dao Liu <daoliu@amazon.com>
2018-04-20 14:27:43 +0800

    remove info

--- js/landing_page.js
+++ js/landing_page.js
@@ -22,7 +22,6 @@ $(window).on('gesture-detected', function (event, gesture) {
         window.history.go(-1);
     }
     else if (gesture === "DOWN WAVE" || gesture === "DOWN" ) {
-        window.location.href = './landing_page.html';
     }
     else if (gesture === "SHAKING") {
         reload();
