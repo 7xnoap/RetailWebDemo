edeef853e85b07cdae73e16d519355018b01ac00
Dao Liu <daoliu@amazon.com>
2018-04-20 14:31:51 +0800

    remove info

--- js/landing_page.js
+++ js/landing_page.js
@@ -11,7 +11,7 @@ $('.gw-card').click(function () {
 });
 
 $(window).on('gesture-detected', function (event, gesture) {
-    alert(gesture);
+    // alert(gesture);
     if (gesture === "LEFT SHAKE" || gesture === "LEFT SWIPE" ) {
         prev();
     }
